a4903410de44e6f260c953923c3a65a2a47d7c0c
古郡 まさき <108216941+gotouerina@users.noreply.github.com>
2022-10-17 14:52:53 +0800

    Update batch_standard.sh

--- batch_standard.sh
+++ batch_standard.sh
@@ -1,13 +1,13 @@
 ##Usage : sh batch.sh
 ##Last batch file name = batch_run.sh
-read -p "Please write the inputfile prefix(example:fa):" prefix
+read -p "Please write the inputfile suffix(example:fa):" prefix
 for i in *.$prefix
 do
 	echo "$i" >> batch_file
 done
 echo "Please write the constant1 and constant2"
 python3 batch_standard.py > batch_run.sh
-read -p "Please write the outputfile prefix(example:bam): " pre
+read -p "Please write the outputfile suffix(example:bam): " pre
 sed -i "s/\.$prefix.out/\.$pre /g" batch_run.sh
 ##len=`cat batch_run.sh | wc -l`
 ##ParaFly -c batch_run.sh -CPU $len
