bd2a708943b70cb65b253ec5205d8f7973870bc0
古郡 まさき <108216941+gotouerina@users.noreply.github.com>
2022-10-18 15:31:55 +0800

    Update tsvc.sh

--- src/tsvc.sh
+++ src/tsvc.sh
@@ -1,6 +1,7 @@
 ##从ENA下载tsv_txt批量下载数据
 ##Usage sh tsv.sh
 cat example.tsv.txt   | awk '{print $8}' | tr -s ';' '\n'  | sed -e '1d' > batch_file
-python3 batch_noneout.py > download.sh
+x=`pwd`
+$x/batch  > batch_run.sh
 ##t=`cat download.sh | wc -l`
 ##ParaFly -c download.sh  -CPU 
