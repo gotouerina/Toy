9496a4739c6ce73bd0de480270a9e4c452b44f5f
古郡 まさき <108216941+gotouerina@users.noreply.github.com>
2023-06-20 20:03:49 +0800

    Delete tsv.sh

--- tsv.sh
+++
@@ -1,8 +0,0 @@
-##从ENA下载tsvtxt批量下载数据
-##Constant1 = wget Contant2 = 
-##Usage sh tsv.sh
-cat example.tsv.txt   | awk '{print $8}' | tr -s ';' '\n'  | sed -e '1d' > batch_file
-echo "Please write the constant1 and constant2"
-python3 batch_noneout.py > download.sh
-##t=`cat download.sh | wc -l`
-##ParaFly -c download.sh  -CPU && rm batch_run.sh && rm batch_file
